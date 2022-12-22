dd0f1b859a35c951bf06fde8b8e35dfe4a72eac5
EltonBa <eltonbajraliu7@gmail.com>
2022-12-22 11:49:05 +0200

    new updates

---
+++ script.js
@@ -0,0 +1,26 @@
+// var vargu = [
+
+//     'images/brand1.png',
+//     'images/brand2.png',
+//     'images/brand3.png',
+//     'images/brand4.png',
+//     'images/brand5.png',
+//     'images/brand6.png'
+// ];
+
+// var index = 0;
+// const koha = 1500;
+
+// function Slider(){
+
+//     document.getElementById('sponsors').src = vargu[index];
+//     index++;
+
+//     if(index == vargu.length){
+//         index=0;
+//     }
+
+//     setTimeout("Slider()",koha)
+// }
+
+// Slider();
